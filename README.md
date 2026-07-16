a10a0827b87c95d4d2871655a6f278f4becc9fca
mahmoodaldoum-cpu <mahmoodaldoum@gmail.com>
2026-07-17 00:52:55 +0300

    Fix PostgreSQL setup and database connection

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# voice_auth_project
+# Voice Authentication Backend
+
+Student 2 Backend
+
+Technologies:
+
+- FastAPI
+- PostgreSQL
+- SQLAlchemy
+- Passlib
+- JWT Authentication (coming soon)
+
+Completed:
+
+- Project setup
+- PostgreSQL connection
+- Users table
+- Registration endpoint
+- Password hashing
--- app/database/connection.py
+++ app/database/connection.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Change the password if yours is different
-DATABASE_URL = "postgresql://postgres:VoiceAuth123@localhost:5432/voice_auth"
+DATABASE_URL = "postgresql://postgres:1234@localhost:5432/voice_auth"
+
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
